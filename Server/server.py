3009de95956b4e5b44eb731219e5b57f151727a7
Shrek231 <49622239+shrek231@users.noreply.github.com>
2020-11-06 21:14:06 -0800

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -8,10 +8,13 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
-    print(data)
-    text = str(data, 'utf-8')
-    file = open("Text.txt", "w")
-    file.write(text + "\n")
-    file.close()
+    try:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("received message: %s" % data)
+        print(data)
+        text = str(data, 'utf-8')
+        file = open("Text.txt", "w")
+        file.write(text + "\n")
+        file.close()
+    except:
+        print("malformed packet")
