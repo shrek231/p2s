c7c25d252b6ac9c86562da95e9796691465218de
Shrek231 <49622239+shrek231@users.noreply.github.com>
2020-11-07 02:23:13 -0800

    Update Client.py

--- Client/Client.py
+++ Client/Client.py
@@ -3,9 +3,6 @@ import time
 import socket
 import threading
 import requests
-
-
-
 def TXThread():
 	while True:
 		try:
@@ -22,44 +19,31 @@ def TXThread():
 			sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 		except:
 			print("cant connect to server, its probubly off")
-
-
-
 def RXThread():
 	a = ""
 	while True:
-		try:
+		#try:
 
-			url = "http://144.202.94.205:8080/text"
+		url = "http://" + (UDP_IP.decode('utf8')) + ":8080/text"
+		#print(url)
 
-			resp = requests.get(url)
+		resp = requests.get(url)
 
-			if (resp.content != a):
-				print("\n\n")
-				print("______________________")
-				#print ("MSG: " + str(MESSAGE))
-				print (resp.content)
-				print("______________________")
-				print("\n\n")
-				a = resp.content
-			time.sleep(1)
-		except:
-			print("cant connect to server api, its probubly off")
-
-
-
+		if (resp.content != a):
+			print("\n\n")
+			print("______________________")
+			#print ("MSG: " + str(MESSAGE))
+			print (resp.content)
+			print("______________________")
+			print("\n\n")
+			a = resp.content
+		time.sleep(1)
+		#except:
+			#print("cant connect to server api, its probubly off")
 # try:
 UDP_IP = bytes(input("IP (default: 144.202.94.205): "),encoding='utf8')
 username = input("Name : ")
-
 threading.Thread(target=TXThread).start()
 threading.Thread(target=RXThread).start()
-
 # except:
 #    print("Error: unable to start thread")
-
-
-
-#####
-
-
